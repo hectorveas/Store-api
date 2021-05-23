e07a49dfe3772a68973336b628b74902ac7c1f67
Hector Veas <hveas10@gmail.com>
2021-05-23 02:47:05 -0400

    Prueba PC

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('categories')
 export class CategoriesController {
+  productService: any;
+
   @Get(':id/product/:productId')
   getCategories(
     @Param('id') id: string,
     @Param('producId') productId: string,
   ): string {
     return `${productId} and ${id}`;
+  }
+
+
+  @Get()
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    //return `products limit=> ${limit} offset ${offset} brand ${brand}`;
+    return this.productService.findAll();
   }
 }
